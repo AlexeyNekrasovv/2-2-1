cb714c39f98dc9b00c2064273a096d47d3c33fb4
AlekseyNekrasovv <ale77221telych@gmail.com>
2024-10-14 15:48:58 +0500

    14.10.1

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -26,4 +26,17 @@ public class UserDaoImp implements UserDao {
       return query.getResultList();
    }
 
+   @Override
+   public User findOwner(String car_name, String car_series) {
+      // Создаем HQL-запрос для поиска пользователя по модели и серии автомобиля
+      String hql = "SELECT u FROM User u JOIN u.car c WHERE c.name = :car_name AND c.series = :car_series";
+      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql, User.class);
+      query.setParameter("car_name", car_name);
+      query.setParameter("car_series", car_series);
+
+      // Выполняем запрос и получаем результат
+      List<User> users = query.getResultList();
+      return users.isEmpty() ? null : users.get(0);
+   }
+
 }
